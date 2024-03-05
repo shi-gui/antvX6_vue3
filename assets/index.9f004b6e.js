5c1c0d93223a2e0b045dac92e1249ec8dec4f68c
shi-gui <33824430+shi-gui@users.noreply.github.com>
2024-03-05 03:49:44 +0000

    Deploying to gh-pages from  @ 41cd8d6ede41ac776150897ba74e52d9e3ce2487 🚀

--- assets/index.9f004b6e.js
+++
@@ -1,1 +0,0 @@
-import{B as n,a as t}from"./button.d9a9bc00.js";n.Group=t;n.install=function(o){return o.component(n.name,n),o.component(t.name,t),o};
---
+++ assets/index.ade3cb69.js
@@ -0,0 +1,1 @@
+import{B as n,a as t}from"./button.712a3a9c.js";n.Group=t;n.install=function(o){return o.component(n.name,n),o.component(t.name,t),o};
